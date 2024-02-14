95d32f885ca30ecc4a9c516e4b3bb36613629355
xJustFox <dodydaversa@gmail.com>
2024-02-14 15:54:18 +0100

    [Upgrade] Added style to cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-    <main class="bg-light">
-        <div class="container-lg py-5 ">
-            <div class="row">
-                @foreach ($movies as $movie)
-                    <div class="col-3">
-                        <ul class=" list-unstyled border border-1 border-black p-2 text-center ">
-                            <li>Title: {{$movie->title}}</li>
-                            <li>Original Title: {{$movie->original_title}}</li>
-                            <li>Nationality: {{$movie->nationality}}</li>
-                            <li>Release: {{$movie->date}}</li>
-                            <li>Vote: {{$movie->vote}}</li>
-                        </ul>
+@section('content')
+    <div class="container-lg py-5 ">
+        <div class="row justify-content-center ">
+            @foreach ($movies as $movie)
+                <div class="col-3 my-3 ">
+                    <div class="my-card">
+                        <div>{{ $movie->title }}</div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
-    </main>
-
-</body>
-
-</html>
+    </div>
+@endsection
