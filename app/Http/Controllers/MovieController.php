c79ca1205c274dd97afb99277d55fbd7538a1456
xJustFox <dodydaversa@gmail.com>
2024-02-14 15:07:29 +0100

    [Upgrade] Connection made

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        return view('home');
+    }
 }
